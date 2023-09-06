ed8d4b85dcdb67eeaa2711278525468b00ccbb48
Natalia Smirnova <nsmirnova@intellective.com>
2023-09-06 13:22:20 +0300

    Hello from Natalia Smirnova

---
+++ src/test/java/HelloFromSmirnovaTest.java
@@ -0,0 +1,8 @@
+import org.junit.jupiter.api.Test;
+
+public class HelloFromSmirnovaTest {
+    @Test
+    public void helloFrom(){
+        System.out.println("Hello from Natalia Smirnova");
+    }
+}
