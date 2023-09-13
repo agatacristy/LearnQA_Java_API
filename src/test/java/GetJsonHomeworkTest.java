06878d3631624dcf6c792335664cee1ab860d73e
Natalia Smirnova <nsmirnova@intellective.com>
2023-09-13 13:42:51 +0300

    Ex5: JSON parsing

--- src/test/java/GetJsonHomeworkTest.java
+++ src/test/java/GetJsonHomeworkTest.java
@@ -2,10 +2,6 @@ import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.Test;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-
 public class GetJsonHomeworkTest {
     @Test
     public void testGetJsonHomeworkTest(){
